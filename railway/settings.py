3238855eb8682a30579176a937756dedbb47c436
Yash Mistry <yash.mistry.g43@gmail.com>
2023-05-07 10:52:40 +0530

    Update settings.py

--- railway/settings.py
+++ railway/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'hackathon.apps.HackathonConfig',
+    'hackathon',
     'django.contrib.staticfiles',
 ]
 
